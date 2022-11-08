d4280c89260f713e7e3416eff94f196c045ea436
Francisco de Assis <franciscodeassisc.santos@gmail.com>
2022-11-08 16:23:27 -0300

    refactor: use react router dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
 import { ThemeProvider } from "styled-components";
 import { GlobalStyle } from "./styles/global";
 import { defaultTheme } from "./styles/theme/default";
+import { Route, Routes } from "react-router-dom";
+import { Home } from "./pages/Home";
+import { History } from "./pages/History";
+import { DefaultLayout } from "./layouts/DefaultLayout";
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <div>Global</div>
+      <Routes>
+        <Route path="/" element={<DefaultLayout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/history" element={<History />} />
+        </Route>
+      </Routes>
       <GlobalStyle />
     </ThemeProvider>
   );
