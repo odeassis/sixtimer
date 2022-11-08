d8c49c5519703640ace313922e26e33b2e2456a0
Francisco de Assis <franciscodeassisc.santos@gmail.com>
2022-11-08 10:33:37 -0300

    setting up the project structure

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { ThemeProvider } from "styled-components";
+import { GlobalStyle } from "./styles/global";
+import { defaultTheme } from "./styles/theme/default";
+
+export function App() {
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <div>Global</div>
+      <GlobalStyle />
+    </ThemeProvider>
+  );
+}
