83b85edd144486122381061f33d24e7f84f76b38
Francisco de Assis <franciscodeassisc.santos@gmail.com>
2022-11-10 22:48:39 -0300

    feat: create inputs and button component

--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -29,6 +29,41 @@ export const FormContainer = styled.div`
   flex-wrap: wrap;
 
 `;
+
+const InputStyle = styled.input`
+
+  background: transparent;
+  height: 2rem;
+  border: 0;
+  border-bottom: 2px solid ${props => props.theme["gray-500"]};
+  color: ${props => props.theme["gray-300"]};
+  font-size: 1.125rem;
+  padding: 0 0.5rem;
+
+  &:focus{
+    box-shadow: none;
+    border-color: ${props => props.theme["green-500"]}
+  }
+
+  &::placeholder {
+    color: ${props => props.theme["gray-500"]};
+  }
+
+`;
+
+export const TaskInput = styled(InputStyle)`
+  flex: 1;
+
+  &::-webkit-calendar-picker-indicator {
+    display: none !important;
+  }
+`;
+
+export const MinutesAmountInput = styled(InputStyle)`
+  width: 4rem;
+`;
+
+
 export const CounterContainer = styled.div`
   display: flex;
   font-family: 'Roboto Mono', monospace;
@@ -51,4 +86,35 @@ export const Separator = styled.div`
   justify-content:center;
 
   color: ${props => props.theme["green-500"]}
+`;
+
+export const StartCounterButton = styled.button`
+  width: 100%;
+  height: 2.5rem;
+  border: none;
+
+  background: ${props => props.theme["green-500"]};
+  color: ${props => props.theme["gray-100"]};
+
+  font-size: 1.121rem;
+  font-weight: bold;
+
+  display: flex;
+  align-items: center;
+  justify-content:center;
+  gap: 1rem;
+
+  border-radius: 8px;
+  cursor: pointer;
+
+  transition: background 0.5s;
+
+  &:disabled{
+    opacity: 0.7;
+    cursor: not-allowed;
+  }
+
+  &:not(:disabled):hover{
+    background: ${props => props.theme["green-700"]};
+  }
 `;
