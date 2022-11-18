2563b75bc7bbfeb444f48ba2c2e42d5b4d4c68b9
Francisco de Assis <franciscodeassisc.santos@gmail.com>
2022-11-17 22:32:19 -0300

    feat: validate form

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,6 +1,11 @@
 import { Play } from "phosphor-react";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import * as zod from "zod";
+
 import {
   CounterContainer,
+  ErrorContainer,
   FormContainer,
   HomeContainer,
   MinutesAmountInput,
@@ -8,11 +13,38 @@ import {
   StartCounterButton,
   TaskInput,
 } from "./styles";
+import { MessageError } from "../../components/MessageError";
+
+const newCycleFormValidationSchema = zod.object({
+  task: zod.string().min(3, "A tarefa deve ter no mínimo 3 letras."),
+  minutesAmount: zod
+    .number()
+    .min(5, "No mínimo 5 minutos.")
+    .max(60, "No máximo 60 minutos.")
+    .multipleOf(5, "Somente múltiplos de 5 (5, 10, 15 ...)"),
+});
+
+type NewCycleFormData = zod.infer<typeof newCycleFormValidationSchema>;
 
 export function Home() {
+  const { register, handleSubmit, watch, formState, reset } = useForm({
+    resolver: zodResolver(newCycleFormValidationSchema),
+    defaultValues: {
+      task: "",
+      minutesAmount: 0,
+    },
+  });
+
+  function handleCreateNewCycle() {
+    reset();
+  }
+
+  const task = watch("task");
+  const isSubmitDisabled = !task;
+
   return (
     <HomeContainer>
-      <form action="">
+      <form onSubmit={handleSubmit(handleCreateNewCycle)}>
         <FormContainer>
           <label htmlFor="task">Vou trabalhar em</label>
           <TaskInput
@@ -20,6 +52,7 @@ export function Home() {
             id="task"
             list="task-suggestions"
             placeholder="Dê um nome para sua tarefa"
+            {...register("task")}
           />
 
           <datalist id="task-suggestions">
@@ -37,6 +70,9 @@ export function Home() {
             min={5}
             step={5}
             placeholder="00"
+            {...register("minutesAmount", {
+              valueAsNumber: true,
+            })}
           />
 
           <span>minutos</span>
@@ -49,11 +85,19 @@ export function Home() {
           <span>0</span>
         </CounterContainer>
 
-        <StartCounterButton type="submit" disabled>
+        <StartCounterButton type="submit" disabled={isSubmitDisabled}>
           <Play size={24} />
           Iniciar
         </StartCounterButton>
       </form>
+      <ErrorContainer>
+        {Object.entries(formState.errors).map((key, value) => (
+          <MessageError
+            key={key[1]?.message?.toString()}
+            message={key[1]?.message?.toString()}
+          />
+        ))}
+      </ErrorContainer>
     </HomeContainer>
   );
 }
