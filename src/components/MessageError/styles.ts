e122dd696dbfdfcdd953b85083665a6b3429b748
Francisco de Assis <franciscodeassisc.santos@gmail.com>
2022-11-21 22:53:46 -0300

    feat: create background to msg error

--- src/components/MessageError/styles.ts
+++ src/components/MessageError/styles.ts
@@ -3,22 +3,31 @@ import styled from "styled-components";
 export const MessageErrorContainer = styled.div`
   //visibility: hidden;
   //max-height: 0;
+  width: 100%;
   display: flex;
   flex: 1;
   align-items: center;
 
+  background: ${props => props.theme['gray-700']};
+  border: 2px solid ${props => props.theme['red-500']};
+  padding: 0.8rem;
+  border-radius: 8px;
 
   gap: 1.5rem;
 
-  svg, span{
-    color: ${props => props.theme["red-500"]}
+  svg {
+    color: ${props => props.theme['red-500']}
+  }
+
+  span{
+    color: ${props => props.theme["white"]}
   }
 
   span {
     font-size: 1.1rem;
   }
 
-  &:disabled {
-
+  &:first-child{
+    margin-bottom: 0.75rem;
   }
 `;
