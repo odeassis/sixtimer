964f6d886889163cb1bd208d844f8cc46aa304e0
Francisco de Assis <franciscodeassisc.santos@gmail.com>
2022-11-22 22:38:29 -0300

    refactor(App): separate routes file

--- src/App.tsx
+++ src/App.tsx
@@ -5,16 +5,15 @@ import { Route, Routes } from "react-router-dom";
 import { Home } from "./pages/Home";
 import { History } from "./pages/History";
 import { DefaultLayout } from "./layouts/DefaultLayout";
+import { Router } from "./Router";
+import { CyclesContextProvider } from "./contexts/CycleContext";
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <Routes>
-        <Route path="/" element={<DefaultLayout />}>
-          <Route path="/" element={<Home />} />
-          <Route path="/history" element={<History />} />
-        </Route>
-      </Routes>
+      <CyclesContextProvider>
+        <Router />
+      </CyclesContextProvider>
       <GlobalStyle />
     </ThemeProvider>
   );
